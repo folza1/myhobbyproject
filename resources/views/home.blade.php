9373ea59bd3180e20dda33498715e0b310576d45
Zoltan Fodor <fodorzoltan317@gmail.com>
2023-04-18 16:11:00 +0200

    Chart_Works_Weekdays_Layout_ScreenSize

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Write in your weight!</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">Write in your weight!</div>
 
-                <div class="card-body">
-                    <form method="post" action="/home">
-                        @csrf
-                        <div>
-                            <label for="weight">Weight:</label>
-                            <input type="number" step="0.01" min="0" name="weight" id="weight" required class="form-control mb-3">
-                        </div>
-                        <button type="submit">Submit</button>
-                    </form>
+                    <div class="card-body">
+                        <form method="post" action="/home">
+                            @csrf
+                            <div>
+                                <label for="weight">Weight:</label>
+                                <input type="number" step="0.01" min="0" name="weight" id="weight" required
+                                       class="form-control mb-3" autofocus>
+                            </div>
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<style>
-    #chartDiv{
-        width: 65%;
-        background: #f1f1f1;
-    }
-</style>
-<div id="chartDiv" class="p-5 mx-auto mt-4 mb-4">
-<canvas id="myChart"></canvas>
-</div>
+    <style>
+        /* set the form width to 100% under 700px screen */
+        #chartDiv {
+            background: #f1f1f1;
+        }
 
-<script>
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: {!! json_encode($weights->pluck('created_at')) !!},
-            datasets: [{
-                label: 'Weight',
-                data: {!! json_encode($weights->pluck('weight')) !!},
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
+        @media screen and (max-width: 700px) {
+            #chartDiv {
+                width: 100%;
             }
         }
-    });
-</script>
+    </style>
+    <div class="container">
+        <div id="chartDiv" class="mx-auto mt-4 mb-4 col-md-10">
+            <canvas id="myChart"></canvas>
+        </div>
+    </div>
+
+    <script>
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart;
+
+        function createChart() {
+            if (chart) {
+                chart.destroy(); // destroy existing chart instance
+            }
+            chart = new Chart(ctx, {
+                // updated chart configuration
+                type: 'line',
+                data: {
+                    labels: {!! json_encode($weights->pluck('created_at')->map(function ($date) { return \Carbon\Carbon::parse($date)->format('D M d'); })) !!},
+                    datasets: [{
+                        label: 'Weight',
+                        data: {!! json_encode($weights->pluck('weight')) !!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'green',
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }],
+                        xAxes: [{
+                            type: 'category'
+                        }]
+                    }
+                }
+            });
+        }
+
+        // function to refresh chart on window resize
+        function refreshChart() {
+            createChart();
+        }
+
+        // add event listener for window resize
+        window.addEventListener('resize', refreshChart);
+
+        // create chart on page load
+        window.addEventListener('load', createChart);
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Weight Loss-Gain App</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
